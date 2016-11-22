c466fd19cd1d450f20ef3452e95372a438a1cd1f
yucong02 <yucong02@baidu.com>
2016-11-22 15:01:53 +0800

    from bash

--- api.js
+++ api.js
@@ -44,7 +44,7 @@ function getUserAllInfo(id) {
 				return localIp().then(ip=>{
 					var path = u.img_path
 					delete u.img_path
-					return Object.assign(u, info, {img: "http://"+ip+':'+process.env.PORT+'/users/'+path})
+					return Object.assign(u, info, {img: "http://"+ip+'/users/'+path})
 				})
 			}
 			delete u.img_path
